1bd421a3da0cc89e2f6e1d1b4ef2d134efb87d34
ann <114505124+paramecium-b@users.noreply.github.com>
2022-12-03 19:35:01 -0500

    entry 8 update

---
+++ entries/entry8/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+    $("#box1").click(function() {
+        $("#box2").toggle();
+    })
+})
+
+$(document).ready(function() {
+    $("#box2").click(function() {
+        $("#box3").toggle();
+    })
+})
+
+$(document).ready(function() {
+    $("#box2").click(function() {
+        $("#box4").toggle();
+    })
+})
+
+$(document).ready(function() {
+    $("#box4").click(function() {
+        $("#box5").toggle();
+    })
+})
+
+$(document).ready(function() {
+    $("#box4").click(function() {
+        $("#box7").toggle();
+    })
+})
+
+$(document).ready(function() {
+    $("#box4").click(function() {
+        $("#box8").toggle();
+    })
+})
+
+$(document).ready(function() {
+    $("#box7").click(function() {
+        $("#box6").toggle();
+    })
+})
+
+$(document).ready(function() {
+    $("#box8").click(function() {
+        $("#box9").toggle();
+    })
+})
+
+$(document).ready(function() {
+    $("#box8").click(function() {
+        $("#box10").toggle();
+    })
+})
+
+$(document).ready(function() {
+    $("#box9").click(function() {
+        $("#box13").toggle();
+    })
+})
+
+$(document).ready(function() {
+    $("#box3").click(function() {
+        $("#box12").toggle();
+    })
+})
+
+$(document).ready(function() {
+    $("#box10").click(function() {
+        $("#box11").toggle();
+    })
+})
